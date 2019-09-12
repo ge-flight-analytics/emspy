4a252f3410efbdb3be23132bddb0983569b36a52
Abhishek.Routray <abhishek.routray@ge.com>
2019-09-12 11:39:39 -0500

    Move MockConnection objects into individual tests

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -5,16 +5,6 @@ from unittest.mock import patch
 from mock_connection import MockConnection
 from mock_ems import MockEMS
 
-sys = 'ems24-app'
-
-user_name = ''
-pwd = ''
-proxies = {
-    'http': '',
-    'https': ''
-}
-
-c = MockConnection(user=user_name, pwd=pwd, proxies=proxies)
 
 # @patch() decorator replaces an object with a mock object for the test it decorates
 # For more information on how to use @patch, see https://stackoverflow.com/questions/32461465/python-patch-not-working
@@ -22,6 +12,8 @@ c = MockConnection(user=user_name, pwd=pwd, proxies=proxies)
 @patch('emspy.Connection', MockConnection)
 def test_excess_profile_matches_by_profile_name():
     try:
+        sys = 'ems24-app'
+        c = MockConnection(user='', pwd='')
         # There are multiple profiles with the exact name "Duplicate Profile" in the mocked data set.
         p = Profile(c, sys, profile_name='Duplicate Profile')
         assert False  # should never get here.
@@ -33,6 +25,8 @@ def test_excess_profile_matches_by_profile_name():
 @patch('emspy.Connection', MockConnection)
 def test_no_profile_matches_by_profile_name():
     try:
+        sys = 'ems24-app'
+        c = MockConnection(user='', pwd='')
         # Obviously a profile that would never exist.
         p = Profile(c, sys, profile_name='this profile would never exist 1 2 3 4')  # no profile should match.
         assert False  # should never get here.
@@ -44,6 +38,8 @@ def test_no_profile_matches_by_profile_name():
 @patch('emspy.Connection', MockConnection)
 def test_no_profile_matches_by_profile_number():
     try:
+        sys = 'ems24-app'
+        c = MockConnection(user='', pwd='')
         # Obviously a profile that would never exist.
         p = Profile(c, sys, profile_number=1000000000000000000000000000)  # no profile should match.
         assert False # should never get here.
@@ -54,6 +50,8 @@ def test_no_profile_matches_by_profile_number():
 @patch('emspy.query.query.EMS', MockEMS)
 @patch('emspy.Connection', MockConnection)
 def test_one_profile_match_by_profile_name():
+    sys = 'ems24-app'
+    c = MockConnection(user='', pwd='')
     p = Profile(c, sys, profile_name='Single Real Profile')  # no profile should match.
     assert (p._profile_name == 'Single Real Profile')
 
@@ -61,6 +59,8 @@ def test_one_profile_match_by_profile_name():
 @patch('emspy.query.query.EMS', MockEMS)
 @patch('emspy.Connection', MockConnection)
 def test_one_profile_match_by_profile_number():
+    sys = 'ems24-app'
+    c = MockConnection(user='', pwd='')
     p = Profile(c, sys, profile_number=108)  # one profile should match.
     assert (p._profile_name == 'Single Real Profile 2')
 
@@ -68,6 +68,8 @@ def test_one_profile_match_by_profile_number():
 @patch('emspy.query.query.EMS', MockEMS)
 @patch('emspy.Connection', MockConnection)
 def test_profile_attributes_by_profile_number():
+    sys = 'ems24-app'
+    c = MockConnection(user='', pwd='')
     p = Profile(c, sys, profile_number=88)  # one profile should match.
     assert (p._profile_name == 'Duplicate Profile')
     assert (p._guid == '5b8dc8cb-c8cb-c8cb-c8cb-c8cb39e6c8cb')
@@ -79,6 +81,8 @@ def test_profile_attributes_by_profile_number():
 @patch('emspy.query.query.EMS', MockEMS)
 @patch('emspy.Connection', MockConnection)
 def test_profile_attributes_by_profile_name():
+    sys = 'ems24-app'
+    c = MockConnection(user='', pwd='')
     p = Profile(c, sys, profile_name='Single Profile 3')  # one profile should match.
     assert (p._profile_name == 'Single Profile 3')
     assert (p._guid == 'f163eeee-63ee-63ee-63ee-1b363eed63ee')
@@ -91,6 +95,8 @@ def test_profile_attributes_by_profile_name():
 @patch('emspy.Connection', MockConnection)
 def test_bad_profile_name_search():
     try:
+        sys = 'ems24-app'
+        c = MockConnection(user='', pwd='')
         p = Profile(c, sys, profile_name='A PROFILE THAT SHOULD NEVER EXIST')  # no profiles should match.
         assert False
     except LookupError:
