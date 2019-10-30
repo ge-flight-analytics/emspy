4c118789b8332ded7014c9ed0d676bf1ddeecb09
Cody Owens <owenscody@gmail.com>
2019-10-29 19:15:35 -0500

    Minor improvements to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(name = 'emspy',
       version = '0.2.0',
       description = 'A Python EMS RESTful API Client/Wrapper',
+      long_description="README.md",
+      long_description_content_type="text/markdown",
       classifiers = [
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
