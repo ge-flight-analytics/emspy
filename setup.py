3ae711f563a14e721a7acd8a76d634a56b4f86e3
Cody Owens <owenscody@gmail.com>
2019-10-29 19:33:53 -0500

    More setup.py, add release steps

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name = 'emspy',
       version = '0.2.0',
       description = 'A Python EMS RESTful API Client/Wrapper',
-      long_description="README.md",
+      long_description=readme(),
       long_description_content_type="text/markdown",
       classifiers = [
         'Programming Language :: Python :: 2.7',
