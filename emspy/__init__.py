984544ae95032e0ab291610637cfd14642f84755
Cody Owens <cody.owens@ge.com>
2019-12-17 11:57:58 -0600

    Bump version: 0.2.3 → 0.2.4

--- emspy/__init__.py
+++ emspy/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import
 from .connection import Connection 
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name = 'emspy',
-      version = '0.2.3',
+      version = '0.2.4',
       description = 'A Python EMS RESTful API Client/Wrapper',
       long_description=readme(),
       long_description_content_type="text/markdown",
