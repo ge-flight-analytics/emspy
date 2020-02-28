c1e90aac1d41fcb0d2ec15eb1d5be82aec38a93e
Cody Owens <cody.owens@ge.com>
2020-02-28 09:45:03 -0600

    Use a version range for numpy on 2.7

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
+import sys
 from setuptools import setup, find_packages
 
 def readme():
     with open('README.md') as f:
         return f.read()
+    
+min_numpy_ver = "1.13.3"
+max_numpy_ver = ""
+if sys.version_info < (3, 0):
+    max_numpy_ver = "1.17"
+    
+numpy_install_rule = "numpy >= {numpy_ver}".format(numpy_ver=min_numpy_ver)
+if max_numpy_ver:
+    numpy_install_rule += ", < {numpy_ver}".format(numpy_ver=max_numpy_ver)
 
 setup(name = 'emspy',
       version = '0.2.4',
@@ -19,6 +29,6 @@ setup(name = 'emspy',
       author_email = 'AviationAdiSupport@ge.com',
       license = 'MIT',
       packages = find_packages(exclude=("docs","tests")),
-      install_requires = ['numpy', 'pandas==0.24.1', 'future', 'networkx'],
+      install_requires = [numpy_install_rule, 'pandas', 'future', 'networkx'],
       zip_safe = False)
       
