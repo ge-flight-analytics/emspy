8ffdea9696d848883ef85dd887aea2664b4598b1
patstout <pat2stout@gmail.com>
2020-03-02 10:55:06 -0600

    replace deprecated argmin with idxmin (#41)
    * replace deprecated argmin with idxmin
    
    * Use a specific version of pandas for 2.7 compat

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
+import sys
 from setuptools import setup, find_packages
 
 def readme():
     with open('README.md') as f:
         return f.read()
+
+# Numpy dropped support for python 2.7 in 1.17
+# Pandas dropped support for python 2.7 after 0.24.0
+min_numpy_ver = "1.13.3"
+max_numpy_ver = ""
+pandas_install_rule = 'pandas'
+if sys.version_info < (3, 0):
+    max_numpy_ver = "1.17"
+    pandas_install_rule += "<=0.24.0"
+    
+numpy_install_rule = "numpy >= {numpy_ver}".format(numpy_ver=min_numpy_ver)
+if max_numpy_ver:
+    numpy_install_rule += ", < {numpy_ver}".format(numpy_ver=max_numpy_ver)
 
 setup(name = 'emspy',
       version = '0.2.4',
@@ -19,6 +33,6 @@ setup(name = 'emspy',
       author_email = 'AviationAdiSupport@ge.com',
       license = 'MIT',
       packages = find_packages(exclude=("docs","tests")),
-      install_requires = ['numpy', 'pandas', 'future', 'networkx'],
+      install_requires = [numpy_install_rule, pandas_install_rule, 'future', 'networkx'],
       zip_safe = False)
       
