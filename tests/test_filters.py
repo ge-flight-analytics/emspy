0746abde26d4e45d51822c05305b6515f9d1aec0
212415648 <rodrigo.deluna@ge.com>
2020-07-09 13:54:39 -0500

    Switched test_filters to pytest

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 from mock_connection import MockConnection
 from mock_query import MockFltQuery
 
@@ -32,182 +32,181 @@ def get_filter(query):
     return query._FltQuery__queryset['filter']['args'][0]['value']
 
 
-class TestNumericFilters(unittest.TestCase):
-    def test_equal(self):
-        query = MockFilterQuery('Flight Record')
-        query.filter("'Flight Record' == '17000'")
-        filter = get_filter(query)
-        assert filter['operator'] == 'equal'
-        assert filter['args'][0] == {
-            'type': 'field',
-            'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
 
-        }
-        assert filter['args'][1] == {
-            'type': 'constant',
-            'value': '17000'
-        }
+def test_equal():
+    query = MockFilterQuery('Flight Record')
+    query.filter("'Flight Record' == '17000'")
+    filter = get_filter(query)
+    assert filter['operator'] == 'equal'
+    assert filter['args'][0] == {
+        'type': 'field',
+        'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
 
-    def test_notEqual(self):
-        query = MockFilterQuery('Flight Record')
-        query.filter("'Flight Record' != '17000'")
-        filter = get_filter(query)
-        assert filter['operator'] == 'notEqual'
-        assert filter['args'][0] == {
-            'type': 'field',
-            'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
+    }
+    assert filter['args'][1] == {
+        'type': 'constant',
+        'value': '17000'
+    }
 
-        }
-        assert filter['args'][1] == {
-            'type': 'constant',
-            'value': '17000'
-        }
+def test_notEqual():
+    query = MockFilterQuery('Flight Record')
+    query.filter("'Flight Record' != '17000'")
+    filter = get_filter(query)
+    assert filter['operator'] == 'notEqual'
+    assert filter['args'][0] == {
+        'type': 'field',
+        'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
 
-    def test_greaterThan(self):
-        query = MockFilterQuery('Flight Record')
-        query.filter("'Flight Record' > '17000'")
-        filter = get_filter(query)
-        assert filter['operator'] == 'greaterThan'
-        assert filter['args'][0] == {
-            'type': 'field',
-            'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
+    }
+    assert filter['args'][1] == {
+        'type': 'constant',
+        'value': '17000'
+    }
 
-        }
-        assert filter['args'][1] == {
-            'type': 'constant',
-            'value': '17000'
-        }
+def test_greaterThan():
+    query = MockFilterQuery('Flight Record')
+    query.filter("'Flight Record' > '17000'")
+    filter = get_filter(query)
+    assert filter['operator'] == 'greaterThan'
+    assert filter['args'][0] == {
+        'type': 'field',
+        'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
 
-    def test_greaterThanOrEqual(self):
-        query = MockFilterQuery('Flight Record')
-        query.filter("'Flight Record' >= '17000'")
-        filter = get_filter(query)
-        assert filter['operator'] == 'greaterThanOrEqual'
-        assert filter['args'][0] == {
-            'type': 'field',
-            'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
+    }
+    assert filter['args'][1] == {
+        'type': 'constant',
+        'value': '17000'
+    }
 
-        }
-        assert filter['args'][1] == {
-            'type': 'constant',
-            'value': '17000'
-        }
+def test_greaterThanOrEqual():
+    query = MockFilterQuery('Flight Record')
+    query.filter("'Flight Record' >= '17000'")
+    filter = get_filter(query)
+    assert filter['operator'] == 'greaterThanOrEqual'
+    assert filter['args'][0] == {
+        'type': 'field',
+        'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
 
-    def test_lessThan(self):
-        query = MockFilterQuery('Flight Record')
-        query.filter("'Flight Record' < '17000'")
-        filter = get_filter(query)
-        assert filter['operator'] == 'lessThan'
-        assert filter['args'][0] == {
-            'type': 'field',
-            'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
+    }
+    assert filter['args'][1] == {
+        'type': 'constant',
+        'value': '17000'
+    }
 
-        }
-        assert filter['args'][1] == {
-            'type': 'constant',
-            'value': '17000'
-        }
+def test_lessThan():
+    query = MockFilterQuery('Flight Record')
+    query.filter("'Flight Record' < '17000'")
+    filter = get_filter(query)
+    assert filter['operator'] == 'lessThan'
+    assert filter['args'][0] == {
+        'type': 'field',
+        'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
 
-    def test_lessThanOrEqual(self):
-        query = MockFilterQuery('Flight Record')
-        query.filter("'Flight Record' <= '17000'")
-        filter = get_filter(query)
-        assert filter['operator'] == 'lessThanOrEqual'
-        assert filter['args'][0] == {
-            'type': 'field',
-            'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
+    }
+    assert filter['args'][1] == {
+        'type': 'constant',
+        'value': '17000'
+    }
 
-        }
-        assert filter['args'][1] == {
-            'type': 'constant',
-            'value': '17000'
-        }
+def test_lessThanOrEqual():
+    query = MockFilterQuery('Flight Record')
+    query.filter("'Flight Record' <= '17000'")
+    filter = get_filter(query)
+    assert filter['operator'] == 'lessThanOrEqual'
+    assert filter['args'][0] == {
+        'type': 'field',
+        'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
 
-    def test_notBetweenExclusive(self):
-        query = MockFilterQuery('Flight Record')
-        query.filter("'15000' > 'Flight Record' > '17000'")
-        filter = get_filter(query)
-        assert filter['operator'] == 'notBetweenExclusive'
-        assert filter['args'][0] == {
-            'type': 'field',
-            'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
+    }
+    assert filter['args'][1] == {
+        'type': 'constant',
+        'value': '17000'
+    }
 
-        }
-        assert filter['args'][1] == {
-            'type': 'constant',
-            'value': '15000'
-        }
-        assert filter['args'][2] == {
-            'type': 'constant',
-            'value': '17000'
-        }
+def test_notBetweenExclusive():
+    query = MockFilterQuery('Flight Record')
+    query.filter("'15000' > 'Flight Record' > '17000'")
+    filter = get_filter(query)
+    assert filter['operator'] == 'notBetweenExclusive'
+    assert filter['args'][0] == {
+        'type': 'field',
+        'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
 
-    def test_betweenExclusive(self):
-        query = MockFilterQuery('Flight Record')
-        query.filter("'15000' < 'Flight Record' < '17000'")
-        filter = get_filter(query)
-        assert filter['operator'] == 'betweenExclusive'
-        assert filter['args'][0] == {
-            'type': 'field',
-            'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
+    }
+    assert filter['args'][1] == {
+        'type': 'constant',
+        'value': '15000'
+    }
+    assert filter['args'][2] == {
+        'type': 'constant',
+        'value': '17000'
+    }
 
-        }
-        assert filter['args'][1] == {
-            'type': 'constant',
-            'value': '15000'
-        }
-        assert filter['args'][2] == {
-            'type': 'constant',
-            'value': '17000'
-        }
+def test_betweenExclusive():
+    query = MockFilterQuery('Flight Record')
+    query.filter("'15000' < 'Flight Record' < '17000'")
+    filter = get_filter(query)
+    assert filter['operator'] == 'betweenExclusive'
+    assert filter['args'][0] == {
+        'type': 'field',
+        'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
 
-    def test_betweenInclusive(self):
-        query = MockFilterQuery('Flight Record')
-        query.filter("'15000' <= 'Flight Record' <= '17000'")
-        filter = get_filter(query)
-        assert filter['operator'] == 'betweenInclusive'
-        assert filter['args'][0] == {
-            'type': 'field',
-            'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
+    }
+    assert filter['args'][1] == {
+        'type': 'constant',
+        'value': '15000'
+    }
+    assert filter['args'][2] == {
+        'type': 'constant',
+        'value': '17000'
+    }
 
-        }
-        assert filter['args'][1] == {
-            'type': 'constant',
-            'value': '15000'
-        }
-        assert filter['args'][2] == {
-            'type': 'constant',
-            'value': '17000'
-        }
+def test_betweenInclusive():
+    query = MockFilterQuery('Flight Record')
+    query.filter("'15000' <= 'Flight Record' <= '17000'")
+    filter = get_filter(query)
+    assert filter['operator'] == 'betweenInclusive'
+    assert filter['args'][0] == {
+        'type': 'field',
+        'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
 
-    def test_notBetweenInclusive(self):
-        query = MockFilterQuery('Flight Record')
-        query.filter("'15000' >= 'Flight Record' >= '17000'")
-        filter = get_filter(query)
-        assert filter['operator'] == 'notBetweenInclusive'
-        assert filter['args'][0] == {
-            'type': 'field',
-            'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
+    }
+    assert filter['args'][1] == {
+        'type': 'constant',
+        'value': '15000'
+    }
+    assert filter['args'][2] == {
+        'type': 'constant',
+        'value': '17000'
+    }
 
-        }
-        assert filter['args'][1] == {
-            'type': 'constant',
-            'value': '15000'
-        }
-        assert filter['args'][2] == {
-            'type': 'constant',
-            'value': '17000'
-        }
+def test_notBetweenInclusive():
+    query = MockFilterQuery('Flight Record')
+    query.filter("'15000' >= 'Flight Record' >= '17000'")
+    filter = get_filter(query)
+    assert filter['operator'] == 'notBetweenInclusive'
+    assert filter['args'][0] == {
+        'type': 'field',
+        'value': '[-hub-][field][[[ems-core][entity-type][foqa-flights]][[ems-core][base-field][flight.uid]]]'
 
-    def test_in(self):
-        query = MockFilterQuery('Flight Record')
-        self.assertRaises(ValueError, query.filter, "'Flight Record' in ['17000', '17001', '17002']")
+    }
+    assert filter['args'][1] == {
+        'type': 'constant',
+        'value': '15000'
+    }
+    assert filter['args'][2] == {
+        'type': 'constant',
+        'value': '17000'
+    }
 
-    def test_notIn(self):
-        query = MockFilterQuery('Flight Record')
-        self.assertRaises(ValueError, query.filter, "'Flight Record' not in ['17000', '17001', '17002']")
+def test_in():
+    query = MockFilterQuery('Flight Record')
+    with pytest.raises(ValueError):
+        query.filter("'Flight Record' in ['17000', '17001', '17002']")
 
+def test_notIn():
+    query = MockFilterQuery('Flight Record')
+    with pytest.raises(ValueError):
+        query.filter("'Flight Record' not in ['17000', '17001', '17002']")
 
-if __name__ == '__main__':
-    unittest.main()
 
